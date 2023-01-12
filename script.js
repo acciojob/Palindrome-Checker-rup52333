2eb6b9e58809e3400b5b2defae289216f947cf66
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-01-12 10:52:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
+let newStr=str.toLowerCase().match(/[a-z0-9]/g);
+return newStr.reverse().join('')===newStr.join('');
+	
 }
 module.exports = palindrome
